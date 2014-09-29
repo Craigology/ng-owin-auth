03fbed2728b83ea06e635c6e43bdc6bbe99d9896
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 09:59:30 +1000

    Maybe with moar quotes?

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,7 +1,7 @@
-$binDir = $OctopusParameters["Octopus.Action[Deploy DB].Output.Package.InstallationDirectoryPath"] + '\bin'
+$binDir = $OctopusParameters["Octopus.Action['Deploy DB'].Output.Package.InstallationDirectoryPath"] + '\bin'
 
 write-Host 'Changing directory to: $binDir'
-cd "$binDir"
+cd '$binDir'
 
 write-Host 'Commencing EF migration...'
 & "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
