ada4308a727b953b5dd97a74e0e976106a60c3ff
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 09:22:24 +1000

    The Octopus variable in the Deploy.ps1 was incorrect.

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,5 +1,5 @@
+write-Host 'Changing directory to: Octopus.Action[Deploy DB].Output.Package.InstallationDirectoryPath\bin'
+cd "Octopus.Action[Deploy DB].Output.Package.InstallationDirectoryPath\bin"
+
 write-Host 'Commencing EF migration...'
-
-cd "Octopus.Action[StepName].Output.Package.InstallationDirectoryPath\bin"
-
 & "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
