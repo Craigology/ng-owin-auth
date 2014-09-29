4a60c650d8d300038b00a408de3bd8a187355525
Craig Hunter <CraigHunter@live.com.au>
2014-09-29 16:23:33 +1000

    Parameterised Deploy.ps1 to use Octopus connection string variable.

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,2 +1,5 @@
-write-Host 'Hello'
-& 'c:\CustomTools\ef6.1\migrate.exe'
+write-Host 'Commencing EF migration...'
+
+cd "Octopus.Action[StepName].Output.Package.InstallationDirectoryPath\bin"
+
+& "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
