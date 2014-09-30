30fc7c79f47d82a71185021327794905d5819192
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 11:33:34 +1000

    Line feeds? Really!?

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,4 +1,5 @@
 cd .\bin
 
 write-Host 'Commencing EF migration...'
+
 & "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
