63a421ee23c9992311e4bd1090225a3ce41ba281
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 11:22:24 +1000

    Still issues with variables in the Deploy.ps1, try something different.

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,7 +1,4 @@
-$packageDir = $OctopusParameters["Octopus.Action['Deploy DB'].Output.Package.InstallationDirectoryPath"]
-
-write-Host 'Changing directory to: $packageDir\bin'
-cd '$packageDir\bin'
+cd .\bin
 
 write-Host 'Commencing EF migration...'
 & "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
