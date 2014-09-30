890e08dc6d98a3dc8ed2e2dcc1d34dd964c64df2
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 12:16:51 +1000

    I think I've got it now.

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,7 +1,5 @@
 cd .\bin
 
-cd
-
 write-Host 'Commencing EF migration...'
 
-& "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
+& "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile:..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
