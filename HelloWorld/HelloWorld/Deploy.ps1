0a1eb00684976b613b5866b4c95c6ce17929d5dc
Craig Hunter <CraigHunter@live.com.au>
2014-09-30 11:51:46 +1000

    Changed encoding.

--- HelloWorld/HelloWorld/Deploy.ps1
+++ HelloWorld/HelloWorld/Deploy.ps1
@@ -1,5 +1,7 @@
 cd .\bin
 
+cd
+
 write-Host 'Commencing EF migration...'
 
 & "c:\CustomTools\ef6.1\migrate.exe" HelloWorld.dll /startUpDirectory:. /startUpConfigurationFile..\Web.config /connectionString:"$PrimaryDatabaseConnectionString" /connectionProviderName:System.Data.SqlClient /verbose
