4a3b2ecf451cb997ed04ad3c4d92c3fe147f3e05
Craigology <craig.hunter.au@gmail.com>
2014-11-11 23:36:17 +1000

    Added file missed somehow during repo import.

---
+++ HelloWorld/HelloWorld/Identity/ApplicationSignInManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace HelloWorld.Identity
+{
+    // Configure the application sign-in manager which is used in this application.  
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager)                       
+            : base(userManager, Startup.AuthenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager) UserManager);
+        }
+    }
+}
